ff1c51661f552cb55f1b3be410d6353a283dc4b5
dwyfrequency <jdwy215@gmail.com>
2019-06-22 10:53:58 -0400

    adding functionality to create post

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import LinkList from './LinkList';
 import '../styles/App.css';
+import CreateLink from './CreateLink';
 
 function App() {
-  return <LinkList />;
+  return (
+    <>
+      <LinkList />
+      <CreateLink />
+    </>
+  );
 }
 
 export default App;
