b09f44c13308c9e6feeaed977e3e32497776b75e
dwyfrequency <jdwy215@gmail.com>
2019-06-22 10:57:10 -0400

    adding header and router

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 import LinkList from './LinkList';
 import '../styles/App.css';
 import CreateLink from './CreateLink';
+import Header from './Header';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <LinkList />
-      <CreateLink />
-    </>
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Switch>
+          <Route exact path="/" component={LinkList} />
+          <Route exact path="/create" component={CreateLink} />
+        </Switch>
+      </div>
+    </div>
   );
 }
 
